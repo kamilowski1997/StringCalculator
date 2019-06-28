1766560c38034affb89d532080a5fe3c58802a2a
Kamil Picheta <kamilowski1997@gmail.com>
2019-06-29 01:21:45 +0200

    1st task - up to two numbers separated by comma and added

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,8 +2,24 @@
 public class StringCalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		StringCalculator test = new StringCalculator();
+		String input = new String("2,5");
+		int i = test.Add(input);
+		System.out.println(i);
 	}
 
+	int Add(String numbers) {
+		if(numbers == null||numbers.equals("")) {
+			return 0;
+		}
+		int sum=0;
+		
+		String[] numbers2 = numbers.split(",", 2);
+		
+		for(int i =0; i<numbers2.length;i++) {
+			sum += Integer.parseInt(numbers2[i]);
+		}
+		
+		return sum;
+	}
 }
