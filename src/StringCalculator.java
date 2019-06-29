39a52fa453108f72845c1af859520ca49ee48eb2
Kamil Picheta <kamilowski1997@gmail.com>
2019-06-29 03:10:07 +0200

    4th task - method Add supports default delimeters (optional)

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -4,7 +4,7 @@ public class StringCalculator {
 
 	public static void main(String[] args) {
 		StringCalculator test = new StringCalculator();
-		String input = new String("2,5\n3,100");
+		String input = new String("//h\n2,5\n3,100h900");
 		int i = test.Add(input);
 		System.out.println(i);
 	}
@@ -16,16 +16,36 @@ public class StringCalculator {
 		if( numbers.contains("\n,")||numbers.contains(",\n")) {
 			return 0;
 		}
+		String defaultDelimeter = null;
+		boolean isDefaultDelimeterSet = false;
+		if(numbers.startsWith("//")) {
+			defaultDelimeter = numbers.substring(2,numbers.indexOf("\n"));
+			numbers = numbers.substring(numbers.indexOf("\n")+1);
+			isDefaultDelimeterSet = true;
+
+		}
+		
 		int sum=0;
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		String[] numbers2 = numbers.split(",");
 		
 		
 		for (int i = 0;i<numbers2.length;i++) {	
-			if(numbers2[i].contains("\n")) {
+			if(numbers2[i].contains("\n")||isDefaultDelimeterSet&&numbers2[i].contains(defaultDelimeter)) {
 				String[] numbers3 = numbers2[i].split("\n");
 				for(int j =0; j<numbers3.length;j++) {
-					list.add(Integer.parseInt(numbers3[j]));
+					
+					if(isDefaultDelimeterSet) {
+						if(numbers3[j].contains(defaultDelimeter)) {
+							String[] numbers4 = numbers3[j].split(defaultDelimeter);
+							
+							for(int p =0; p<numbers4.length;p++) {
+								list.add(Integer.parseInt(numbers4[p]));
+							}
+						}else {
+							list.add(Integer.parseInt(numbers3[j]));
+						}
+					}
 				}
 			}else {
 				list.add(Integer.parseInt(numbers2[i]));
@@ -33,6 +53,7 @@ public class StringCalculator {
 		}
 		
 		for(Integer i:list) {
+			System.out.println(i);
 			sum += i;
 		}
 		
