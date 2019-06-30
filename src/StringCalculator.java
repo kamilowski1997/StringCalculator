6da68f50cd62ac3fb4dc2728adbf59ffe8112a37
Kamil Picheta <kamilowski1997@gmail.com>
2019-06-30 18:28:58 +0200

    8th task - multiple delimeters are allowed in format //[delim1][delim2]n

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -4,7 +4,7 @@ public class StringCalculator {
 
 	public static void main(String[] args) {
 		StringCalculator test = new StringCalculator();
-		String input = new String("//[hhh]\n2,5\n3,-100hhh900,1001");
+		String input = new String("//[h][j]\n2,5\n3,-100h900j1000");
 		int i = test.Add(input);
 		System.out.println(i);
 	}
@@ -40,12 +40,13 @@ public class StringCalculator {
 		if( numbers.contains("\n,")||numbers.contains(",\n")) {
 			return 0;
 		}
-		String defaultDelimeter = null;
+		ArrayList<String> defaultDelimeters = new ArrayList<String>();
 		if(numbers.startsWith("//")) {
 			for(int i =2;i<numbers.indexOf("\n");i++) {
 				if(numbers.charAt(i) ==']') {
-					defaultDelimeter = (numbers.substring(3,i));
-					numbers = numbers.substring(0,2)+numbers.substring(i+1);
+					defaultDelimeters.add(numbers.substring(3,4));
+					numbers = numbers.substring(0,2)+numbers.substring(5);
+					i=2;
 				}
 			}
 			numbers = numbers.substring(numbers.indexOf("\n")+1);
@@ -58,8 +59,9 @@ public class StringCalculator {
 		
 
 		numbers2 = Split(numbers2,"\n");
-		numbers2 = Split(numbers2, defaultDelimeter);
-		
+		for(String i:defaultDelimeters) {
+			numbers2 = Split(numbers2, i);
+		}
 		
 		
 		
