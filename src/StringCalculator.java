6f8cea8e3d5732954d4329da697bdcfe1b0b94b8
Kamil Picheta <kamilowski1997@gmail.com>
2019-06-30 18:19:49 +0200

    7th task - delimeters can be any of length and are passed in following format //[delimeter]n

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -4,7 +4,7 @@ public class StringCalculator {
 
 	public static void main(String[] args) {
 		StringCalculator test = new StringCalculator();
-		String input = new String("//h\n2,5\n3,-100h-900,1001");
+		String input = new String("//[hhh]\n2,5\n3,-100hhh900,1001");
 		int i = test.Add(input);
 		System.out.println(i);
 	}
@@ -20,6 +20,19 @@ public class StringCalculator {
 	      }
 	 }
 
+	String[] Split(String[] tab, String delimeter) {
+		ArrayList<String> stringArr = new ArrayList<String>();
+		for (int i = 0;i<tab.length;i++) {	
+			String[] numbers3 = tab[i].split(delimeter);
+			for(int j=0;j<numbers3.length;j++) {
+				stringArr.add(numbers3[j]);
+			}
+		}
+		
+		return stringArr.toArray(new String[stringArr.size()]);
+	}
+	
+	
 	int Add(String numbers) {
 		if(numbers == null||numbers.equals("")) {
 			return 0;
@@ -28,39 +41,30 @@ public class StringCalculator {
 			return 0;
 		}
 		String defaultDelimeter = null;
-		boolean isDefaultDelimeterSet = false;
 		if(numbers.startsWith("//")) {
-			defaultDelimeter = numbers.substring(2,numbers.indexOf("\n"));
+			for(int i =2;i<numbers.indexOf("\n");i++) {
+				if(numbers.charAt(i) ==']') {
+					defaultDelimeter = (numbers.substring(3,i));
+					numbers = numbers.substring(0,2)+numbers.substring(i+1);
+				}
+			}
 			numbers = numbers.substring(numbers.indexOf("\n")+1);
-			isDefaultDelimeterSet = true;
-
 		}
+		
 		
 		int sum=0;
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		String[] numbers2 = numbers.split(",");
 		
+
+		numbers2 = Split(numbers2,"\n");
+		numbers2 = Split(numbers2, defaultDelimeter);
 		
-		for (int i = 0;i<numbers2.length;i++) {	
-			if(numbers2[i].contains("\n")||isDefaultDelimeterSet&&numbers2[i].contains(defaultDelimeter)) {
-				String[] numbers3 = numbers2[i].split("\n");
-				for(int j =0; j<numbers3.length;j++) {
-					
-					if(isDefaultDelimeterSet) {
-						if(numbers3[j].contains(defaultDelimeter)) {
-							String[] numbers4 = numbers3[j].split(defaultDelimeter);
-							
-							for(int p =0; p<numbers4.length;p++) {
-								list.add(Integer.parseInt(numbers4[p]));
-							}
-						}else {
-							list.add(Integer.parseInt(numbers3[j]));
-						}
-					}
-				}
-			}else {
-				list.add(Integer.parseInt(numbers2[i]));
-			}
+		
+		
+		
+		for(String i:numbers2) {
+			list.add(Integer.parseInt(i));
 		}
 		
 		for(Integer i:list) {
