c60a386524d980a053b60fa42a296ee8836dbed9
Kamil Picheta <kamilowski1997@gmail.com>
2019-06-30 18:30:36 +0200

    9th task - multiple delimeters can be also longer than 1 char

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -4,7 +4,7 @@ public class StringCalculator {
 
 	public static void main(String[] args) {
 		StringCalculator test = new StringCalculator();
-		String input = new String("//[h][j]\n2,5\n3,-100h900j1000");
+		String input = new String("//[hhh][j]\n2,5\n3,-100hhh900j1000");
 		int i = test.Add(input);
 		System.out.println(i);
 	}
@@ -44,8 +44,8 @@ public class StringCalculator {
 		if(numbers.startsWith("//")) {
 			for(int i =2;i<numbers.indexOf("\n");i++) {
 				if(numbers.charAt(i) ==']') {
-					defaultDelimeters.add(numbers.substring(3,4));
-					numbers = numbers.substring(0,2)+numbers.substring(5);
+					defaultDelimeters.add(numbers.substring(3,i));
+					numbers = numbers.substring(0,2)+numbers.substring(i+1);
 					i=2;
 				}
 			}
